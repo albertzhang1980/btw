f1cde0c6a2bdb0eb5af484b5ca4b0d47e39366ed
zleil <cfb0d4b6.le@gmail.com>
2013-06-16 08:00:14 +0800

    a

---
+++ TgDataGen/src/com/btiao/tg/datagen/MeiTuanGen.java
@@ -0,0 +1,28 @@
+package com.btiao.tg.datagen;
+
+public class MeiTuanGen extends Gen {
+
+	@Override
+	protected boolean genTg() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected String getName() {
+		return "meituan";
+	}
+
+	@Override
+	protected void preGen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void postGen() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
