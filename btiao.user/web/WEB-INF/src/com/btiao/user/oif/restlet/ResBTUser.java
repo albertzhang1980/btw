a46ef9d687b1ffcc92fe63a4777ec95873d9489e
zleil <cfb0d4b6.le@gmail.com>
2013-07-31 08:17:36 +0800

    a

---
+++ btiao.user/web/WEB-INF/src/com/btiao/user/oif/restlet/ResBTUser.java
@@ -0,0 +1,35 @@
+package com.btiao.user.oif.restlet;
+
+import org.json.JSONObject;
+import org.restlet.data.Form;
+
+import com.btiao.base.exp.BTiaoExp;
+import com.btiao.base.oif.restlet.ResBTBase;
+
+public class ResBTUser extends ResBTBase {
+
+	@Override
+	protected Object get(Form form) throws BTiaoExp {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object put(JSONObject jao) throws BTiaoExp {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object post(JSONObject jao) throws BTiaoExp {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object del(JSONObject jao) throws BTiaoExp {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
