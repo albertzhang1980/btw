99fed52dd27626d13082ffabf1de9ecd1bc3a7fe
zleil <cfb0d4b6.le@gmail.com>
2013-07-31 08:07:47 +0800

    a

--- btiao.user/web/WEB-INF/src/com/btiao/user/oif/restlet/APP.java
+++ btiao.user/web/WEB-INF/src/com/btiao/user/oif/restlet/APP.java
@@ -4,10 +4,12 @@ import org.restlet.Application;
 import org.restlet.Restlet;
 import org.restlet.routing.Router;
 
+import com.btiao.oif.restlet.ResBTBase;
+
 public class APP extends Application {
 	public Restlet createInboundRoot() {
 		Router router = new Router(getContext());
-		router.attach("/users/{userId}", ResBTUser.class);
+		router.attach("/users/{userId}", ResBTBase.class);
 		return router;
 	}
 }
